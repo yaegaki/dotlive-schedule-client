1e2f0b5e708c0b06bf53d8968091113587f269b7
yaegaki <yaegaki.fino@gmail.com>
2020-03-10 21:25:43 +0900

    フォルダ移動とドキュメントフォーマット

--- lib/widgets/schedule_list.dart
+++ lib/widgets/schedule_list.dart
@@ -28,7 +28,8 @@ class _ScheduleListState extends State<ScheduleList> {
 
   Future<void> fetchSchedule() async {
     final jst = widget.date.add(Duration(hours: 9));
-    final url = 'https://dotlive-schedule.appspot.com?q=${jst.year}-${jst.month}-${jst.day}';
+    final url =
+        'https://dotlive-schedule.appspot.com?q=${jst.year}-${jst.month}-${jst.day}';
     _version += 1;
     final temp = _version;
     final res = await http.get(url);
@@ -52,10 +53,9 @@ class _ScheduleListState extends State<ScheduleList> {
       itemCount = 1;
       itemBuilder = (_, __) {
         return Card(
-          child: ListTile(
-            title: Text('予定がありません'),
-          )
-        );
+            child: ListTile(
+          title: Text('予定がありません'),
+        ));
       };
     } else {
       itemCount = _schedule.entries.length;
@@ -75,7 +75,8 @@ class _ScheduleListState extends State<ScheduleList> {
 
   Widget _createCard(ScheduleEntry entry) {
     final d = entry.startAt.add(Duration(hours: 9));
-    final title = '${d.hour.toString().padLeft(2, '0')}:${d.minute.toString().padLeft(2, '0')}~ ${entry.actorName}';
+    final title =
+        '${d.hour.toString().padLeft(2, '0')}:${d.minute.toString().padLeft(2, '0')}~ ${entry.actorName}';
     final icon = entry.icon;
 
     String body;
@@ -83,30 +84,28 @@ class _ScheduleListState extends State<ScheduleList> {
     if (entry.text == '') {
       body = '配信予定';
       hasTrailing = false;
-    }
-    else {
+    } else {
       body = entry.text;
       hasTrailing = true;
     }
 
     return Card(
-      child: ListTile(
-        leading: CircleAvatar(
-          backgroundImage: NetworkImage(icon),
-          maxRadius: 25,
-          minRadius: 25,
-        ),
-        title: Text(title),
-        subtitle: Text(body),
-        isThreeLine: true,
-        trailing: hasTrailing ? Icon(Icons.keyboard_arrow_right) : null,
-        onTap: () async {
-          if (entry.url == '') return;
-          if (await canLaunch(entry.url)) {
-            await launch(entry.url, forceSafariVC: false);
-          }
-        },
-      )
-    );
+        child: ListTile(
+      leading: CircleAvatar(
+        backgroundImage: NetworkImage(icon),
+        maxRadius: 25,
+        minRadius: 25,
+      ),
+      title: Text(title),
+      subtitle: Text(body),
+      isThreeLine: true,
+      trailing: hasTrailing ? Icon(Icons.keyboard_arrow_right) : null,
+      onTap: () async {
+        if (entry.url == '') return;
+        if (await canLaunch(entry.url)) {
+          await launch(entry.url, forceSafariVC: false);
+        }
+      },
+    ));
   }
 }
